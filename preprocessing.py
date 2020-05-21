03c6c1fe070c4464951dbdfc7ef8e9f4d2c186a8
Ali-Omrani <aomrani@usc.edu>
2020-05-20 20:53:39 -0700

    added reading sgts from file and loading GPT-2 model/tokenizer

--- preprocessing.py
+++ preprocessing.py
@@ -10,8 +10,13 @@ class Preprocessing():
     def __init__(self, predict_dir, sgt_path, result_dir):
         self.predict_dir = predict_dir
         self.sgt_file_path = sgt_path
-        self.sgts = read_sgts(sgt_path)
+        with open(sgt_path) as f:
+            content = f.readlines()
+            self.sgts = [item.replace("\n", "") for item in content]
         self.result_dir = result_dir
+        self.tokenizer = torch.hub.load('huggingface/pytorch-transformers', 'tokenizer', 'gpt2')
+        self.model = torch.hub.load('huggingface/pytorch-transformers', 'modelWithLMHead', 'gpt2')
+
 
     def extract_posts_with_sgt(self):
 
